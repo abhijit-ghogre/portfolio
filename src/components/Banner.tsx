3226a7b1d93c115892d2603f71d5ef43d3f6f39b
Abhijit Ghogre <aghogre@whileone.in>
2023-12-28 21:29:25 +0530

    Content update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -26,22 +26,20 @@ function Banner() {
         <div className="mt-4 leading-normal text-lg">
           I am currently spearheading the creation of a global{" "}
           <span className="font-bold">B2B e-commerce</span> marketplace. I have{" "}
-          <span className="font-bold">10+ years</span> of experience building
+          <span className="font-bold">10+ years of experience</span> building
           backend, frontend and infrastructure for various applications and
-          platforms in e-commerce, fleet management and GRC (governance, risk
-          and compliance) domains. I have experience working with{" "}
-          <span className="font-bold">zero-to-one</span> fast paced startups as
-          well as managing SDLC for products running at{" "}
+          platforms in e-commerce - storefronts, seller portals and live
+          commerce. I have built multiple{" "}
+          <span className="font-bold">0-1 products</span> in early stage
+          startups as well as managed products at{" "}
           <span className="font-bold">scale</span>. I like to take up
           challenging problems and convert them into beautiful solutions. I am a
           strong enforcer of good coding standards and writing beautiful code. I
-          mainly use{" "}
-          <span className="font-bold">
-            React.js, Next.js, ReactNative, Vue.js or Svelte
-          </span>{" "}
-          for front end and{" "}
-          <span className="font-bold">Node.js, Laravel or Adonis.js</span> for
-          backend.
+          am a fan of using <span className="font-bold">Javascript</span> based
+          systems for creating applications{" "}
+          <span className="font-bold">(Node.js, React.js, Next.js)</span>. Along
+          with E-Commerce, I have built products in Financial Services, GRC and
+          Fleet Management.
         </div>
         <SocialActions className="mt-4 flex gap-6" />
         <a
